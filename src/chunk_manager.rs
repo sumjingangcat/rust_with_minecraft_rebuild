815b7786347bbbc9b19231a1a3951e4b81d2bb03
sumjingangcat <oj47191@gmail.com>
2024-02-29 00:20:08 +0900

    [fix] not running...

--- src/chunk_manager.rs
+++ src/chunk_manager.rs
@@ -1,29 +1,23 @@
-use crate::chunk;
 use crate::shader::ShaderProgram;
+use crate::UVCoords;
 use crate::{
     chunk::{BlockID, Chunk},
     shapes::write_unit_cube_to_ptr,
-    UVCoords,
 };
+use nalgebra::Matrix4;
+use nalgebra_glm::vec3;
+use noise::{NoiseFn, SuperSimplex};
 use std::borrow::Borrow;
 use std::collections::{HashMap, HashSet};
-
-use nalgebra::{clamp, Matrix4, Vector3};
-use nalgebra_glm::{pi, vec2, vec3, IVec3, Vec2};
-
-use crate::block_texture_sides::{get_uv_every_side, BlockFaces};
-use noise::{NoiseFn, SuperSimplex};
-
+use crate::block_texture_sides::{BlockFaces, get_uv_every_side};
 use rand::random;
 
 pub const CHUNK_SIZE: u32 = 16;
-
 pub const CHUNK_VOLUME: u32 = CHUNK_SIZE * CHUNK_SIZE * CHUNK_SIZE;
 
 pub type Sides = [bool; 6];
 
 pub struct ChunkManager {
-    // 접근을 많이 하므로 Vec 대신 HashMap을 사용한다.
     pub loaded_chunks: HashMap<(i32, i32, i32), Chunk>,
 }
 
@@ -45,8 +39,8 @@ impl ChunkManager {
     }
 
     pub fn simplex(&mut self) {
-        let ss = SuperSimplex::new(31415);
-        let n = 4;
+        let ss = SuperSimplex::new(1296);
+        let n = 10;
 
         for y in 0..16 {
             for z in -n..=n {
@@ -60,43 +54,42 @@ impl ChunkManager {
             for z in -16 * n..16 * n {
                 let (xf, zf) = (x as f64 / 64.0, z as f64 / 64.0);
                 let y = ss.get([xf, zf]);
-                let y = (16.8 * (y + 1.0)) as i32;
+                let y = (16.0 * (y + 1.0)) as i32;
 
                 self.set_block(x, y, z, BlockID::Grass);
                 self.set_block(x, y - 1, z, BlockID::Dirt);
-                self.set_block(x, y - 2, z, BlockID::Cobblestone);
-                self.set_block(x, y - 3, z, BlockID::Obsidian);
+                self.set_block(x, y - 2, z, BlockID::Dirt);
+                self.set_block(x, y - 3, z, BlockID::Cobblestone);
 
-                    if random::<u32>() % 100 == 0 {
-                        let tree_height = 4;
+                if random::<u32>() % 100 == 0 {
+                    let h = 5;
+                    for i in y+1..y+1+h{
+                        self.set_block(x, i, z, BlockID::OakLog)
+                    }
 
-                        for i in y + 1 .. y + 1 + tree_height {
-                            self.set_block(x, i, z, BlockID::OakLog);
-                        }
-
-                        for yy in y + tree_height - 2 ..=y + tree_height - 1 {
-                            for xx in x - 2..= x + 2 {
-                                for zz in z - 2..=z + 2{
-                                    if xx != x || zz != z {
-                                        self.set_block(xx, yy, zz, BlockID::OakLeaves);
-                                    }
-                            }
-                        }
-
-                        for xx in x - 1..= x + 1 {
-                            for zz in z - 1..=z + 1{
-                                if xx != x || zz != z{
-                                    self.set_block(xx, y + tree_height, zz, BlockID::OakLeaves);
+                    for yy in y + h - 2..=y + h - 1{
+                        for xx in x - 2..=x + 2{
+                            for zz in z - 2..=z + 2{
+                                if xx != x || zz != z {
+                                    self.set_block(xx, yy, zz, BlockID::OakLeaves);
                                 }
                             }
                         }
-
-                        self.set_block(x, y + tree_height + 1, z, BlockID::OakLeaves);
-                        self.set_block(x + 1, y + tree_height + 1, z, BlockID::OakLeaves);
-                        self.set_block(x - 1, y + tree_height + 1, z, BlockID::OakLeaves);
-                        self.set_block(x, y + tree_height + 1, z + 1, BlockID::OakLeaves);
-                        self.set_block(x, y + tree_height + 1, z - 1, BlockID::OakLeaves);
                     }
+
+                    for xx in x - 1..=x + 1{
+                        for zz in z - 1..=z + 1{
+                            if xx != x || zz != z {
+                                self.set_block(xx, y + h, zz, BlockID::OakLeaves);
+                            }
+                        }
+                    }
+
+                    self.set_block(x, y + h + 1, z, BlockID::OakLeaves);
+                    self.set_block(x + 1, y + h + 1, z, BlockID::OakLeaves);
+                    self.set_block(x - 1, y + h + 1, z, BlockID::OakLeaves);
+                    self.set_block(x, y + h + 1, z + 1, BlockID::OakLeaves);
+                    self.set_block(x, y + h + 1, z - 1, BlockID::OakLeaves);
                 }
             }
         }
@@ -107,7 +100,7 @@ impl ChunkManager {
         let chunk_y = if y < 0 { (y + 1) / 16 - 1 } else { y / 16 };
         let chunk_z = if z < 0 { (z + 1) / 16 - 1 } else { z / 16 };
 
-        let block_x = x.rem_euclid(16) as u32;
+        let block_x = x.rem_euclid(16) as u32; // (음수) % (양수)의 결과를 0 또는 양수로 고정
         let block_y = y.rem_euclid(16) as u32;
         let block_z = z.rem_euclid(16) as u32;
 
@@ -117,9 +110,9 @@ impl ChunkManager {
     fn get_global_coords(
         (chunk_x, chunk_y, chunk_z, block_x, block_y, block_z): (i32, i32, i32, u32, u32, u32),
     ) -> (i32, i32, i32) {
-        let x = chunk_x * 16 + block_x as i32;
-        let y = chunk_y * 16 + block_y as i32;
-        let z = chunk_z * 16 + block_z as i32;
+        let x = 16 * chunk_x + block_x as i32;
+        let y = 16 * chunk_y + block_y as i32;
+        let z = 16 * chunk_z + block_z as i32;
 
         (x, y, z)
     }
@@ -130,9 +123,7 @@ impl ChunkManager {
 
         self.loaded_chunks
             .get((chunk_x, chunk_y, chunk_z).borrow())
-            .and_then(|chunk| {
-                Some(chunk.get_block(block_x, block_y, block_z))
-            })
+            .and_then(|chunk| Some(chunk.get_block(block_x, block_y, block_z)))
     }
 
     pub fn set_block(&mut self, x: i32, y: i32, z: i32, block: BlockID) {
@@ -141,16 +132,13 @@ impl ChunkManager {
 
         self.loaded_chunks
             .get_mut((chunk_x, chunk_y, chunk_z).borrow())
-            .map(|chunk| {
-                chunk.set_block(block_x, block_y, block_z, block);
-            });
+            .map(|chunk| chunk.set_block(block_x, block_y, block_z, block));
     }
 
     pub fn rebuild_dirty_chunks(&mut self, uv_map: &HashMap<BlockID, BlockFaces<UVCoords>>) {
-        // Purpose : GL Map Buffer를 이용하여 VBO를 업데이트한다.
         let mut dirty_chunks = HashSet::new();
 
-        // Nearby chunks can be also dirty if the change happens at the edge of the chunk.
+        // Nearby chunks can be also dirty if the change happens at the edge
         for (&(x, y, z), chunk) in self.loaded_chunks.iter() {
             if chunk.dirty {
                 dirty_chunks.insert((x, y, z));
@@ -168,20 +156,16 @@ impl ChunkManager {
             let chunk = self.loaded_chunks.get(&coords);
 
             if let Some(chunk) = chunk {
-                let sides_vec = active_sides.entry(coords).or_default(); // if key does not exist, insert default value.
+                let sides_vec = active_sides.entry(coords).or_default();
 
                 for by in 0..CHUNK_SIZE {
                     for bz in 0..CHUNK_SIZE {
                         for bx in 0..CHUNK_SIZE {
-                            if !chunk.get_block(bx, by, bz).is_air() {
-                                let (gx, gy, gz) = 
+                            if !chunk.get_block(bx, by, bz).is_air(){
+                                let (gx, gy, gz) =
                                     ChunkManager::get_global_coords((cx, cy, cz, bx, by, bz));
                                 sides_vec.push(self.get_active_sides_of_block(gx, gy, gz));
                             }
-
-                            let (gx, gy, gz) =
-                                ChunkManager::get_global_coords((cx, cy, cz, bx, by, bz));
-                            sides_vec.push(self.get_active_sides_of_block(gx, gy, gz));
                         }
                     }
                 }
@@ -197,13 +181,12 @@ impl ChunkManager {
                 chunk.vertices_drawn = 0;
 
                 let sides = active_sides.get(coords).unwrap();
-                let n_visible_faces = sides.iter().map(|faces| faces.iter().fold(0, |acc, &x| acc + x as u32)).fold(0, |acc, n| acc + n);
+                let n_visible_faces = sides.iter().map(|faces| faces.iter().fold(0, |acc, &x| acc + x as u32)).fold(0, |acc,  n| acc + n); 
 
                 if n_visible_faces == 0 {
                     continue;
                 }
 
-                // Initialize VBO
                 gl_call!(gl::NamedBufferData(
                     chunk.vbo,
                     (180 * std::mem::size_of::<f32>() * n_visible_faces as usize) as isize,
@@ -211,11 +194,10 @@ impl ChunkManager {
                     gl::DYNAMIC_DRAW
                 ));
 
-                // get buffer object pointer
                 let vbo_ptr = gl_call!(gl::MapNamedBuffer(chunk.vbo, gl::WRITE_ONLY)) as *mut f32;
                 let mut idx = 0;
 
-                let sides_vec: &Vec<Sides> = active_sides.get(&coords).unwrap();
+                let sides_vec = active_sides.get(coords).unwrap();
                 let mut cnt = 0;
 
                 for y in 0..CHUNK_SIZE {
@@ -225,47 +207,21 @@ impl ChunkManager {
 
                             if block != BlockID::Air {
                                 let active_sides = sides_vec[cnt];
+
                                 let uvs = uv_map.get(&block).unwrap().clone();
                                 let uvs = get_uv_every_side(uvs);
 
-                                let copied_vertices = unsafe {
-                                    write_unit_cube_to_ptr(
-                                        vbo_ptr.offset(idx),
-                                        (x as f32, y as f32, z as f32),
-                                        uvs,
-                                        active_sides,
-                                    )
-                                };
+                                let copied_vertices = unsafe { write_unit_cube_to_ptr(
+                                    vbo_ptr.offset(idx), (x as f32, y as f32, z as f32), uvs, active_sides)};
 
                                 chunk.vertices_drawn += copied_vertices;
                                 idx += copied_vertices as isize * 5;
                                 cnt += 1;
-
-                                // let cube_array = unit_cube_array(
-                                //     (x as f32, y as f32, z as f32),
-                                //     uv_bl,
-                                //     uv_tr,
-                                //     active_sides,
-                                // );
-
-                                // gl_call!(gl::NamedBufferSubData(
-                                //     chunk.vbo,
-                                //     (idx * std::mem::size_of::<f32>()) as isize,
-                                //     (cube_array.len() * std::mem::size_of::<f32>()) as isize,
-                                //     cube_array.as_ptr() as *const std::ffi::c_void
-                                // ));
-                                // chunk.vertices_drawn += cube_array.len() as u32 / 5;
-
-                                // idx += cube_array.len();
                             }
                         }
                     }
                 }
-                // say to opengl : that we are done with the buffer
                 gl_call!(gl::UnmapNamedBuffer(chunk.vbo));
-
-                chunk.dirty = false;
-                chunk.dirty_neighbours.clear();
             }
         }
     }
@@ -301,15 +257,14 @@ impl ChunkManager {
 
     pub fn render_loaded_chunks(&mut self, program: &mut ShaderProgram) {
         for ((x, y, z), chunk) in &self.loaded_chunks {
-            // Skip rendering the chunk if there is nothing to draw
+            // skip rendering the chunk if there is nothing to draw
             if chunk.vertices_drawn == 0 {
-                return;
+                continue;
             }
-
             let model_matrix = {
                 let translate_matrix =
                     Matrix4::new_translation(&vec3(*x as f32, *y as f32, *z as f32).scale(16.0));
-                let rotate_matrix = Matrix4::from_euler_angles(0.0, 0.0, 0.0);
+                let rotate_matrix = Matrix4::from_euler_angles(0.0f32, 0.0, 0.0);
                 let scale_matrix = Matrix4::new_nonuniform_scaling(&vec3(1.0f32, 1.0f32, 1.0f32));
 
                 translate_matrix * rotate_matrix * scale_matrix
